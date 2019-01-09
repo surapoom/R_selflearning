8e68fa4937ecbc55fcf9b5b53ab38f873a9aeca3
surapoom <45107078+surapoom@users.noreply.github.com>
2019-01-09 09:06:34 +0700

    adapt to local

--- load_data.R
+++ load_data.R
@@ -24,12 +24,12 @@
 #'                     data_deepth = "14200")
 #' 
 load_data <- function(path_terminal, trade_log_file, time_period = 1, data_deepth = 14200){
-  # path_terminal <- "C:/Program Files (x86)/FxPro - Terminal2/MQL4/Files/"
+  # path_terminal <- "C:/Program Files (x86)/ICMarkets MT4 Terminal2/MQL4/Files/"
   # path_terminal <- file.path(getwd(), "test_data")
   # trade_log_file <- "AI_CP"
   # trade_log_file <- "AI_Macd"
   # time_period <- 1
-  # data_deepth <- "100000"
+  # data_deepth <- "14200"
   require(tidyverse)
   require(lubridate)
   DFT1 <- try(read_csv(file = file.path(path_terminal, paste0(trade_log_file, time_period, "-", data_deepth, ".csv")),
